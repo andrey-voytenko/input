8ccf7f40fd5b4f4e0b136af4f3c038e17d812e53
andrey-voytenko <voytenko.developer@gmail.com>
2023-06-26 23:58:05 +0300

    add a base textarea with a custom placeholder

--- src/app/components/textarea/textarea.component.ts
+++ src/app/components/textarea/textarea.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
-  styleUrls: ['./textarea.component.scss']
+  styleUrls: ['./textarea.component.scss'],
 })
 export class TextareaComponent {
+  @Input() placeholder: string = '';
 
+  public textarea: string = '';
+  public hasFocus: boolean = false;
+
+  public onKey(e: KeyboardEvent) {
+    this.textarea = (<HTMLTextAreaElement>e.target).value;
+  }
+
+  public onBlur() {
+    this.hasFocus = false;
+  }
+
+  public onFocus() {
+    this.hasFocus = true;
+  }
 }
