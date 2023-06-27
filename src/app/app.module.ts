3cbafc1c83b6d33ee5355fd915afebd237c195cf
andrey-voytenko <voytenko.developer@gmail.com>
2023-06-27 18:13:57 +0300

    make the textarea field like a reactive control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextareaComponent } from './components/textarea/textarea.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent,
-    TextareaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TextareaComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/textarea/textarea.component.ts
+++ src/app/components/textarea/textarea.component.ts
@@ -1,18 +1,39 @@
 import { Component, Input, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: TextareaComponent,
+    },
+  ],
 })
-export class TextareaComponent {
+export class TextareaComponent implements ControlValueAccessor {
   @Input() placeholder: string = '';
+  @Input()
+  set value(val) {
+    this._value = val;
+    this.onChange(this._value);
+  }
 
-  public textarea: string = '';
+  get value() {
+    return this._value;
+  }
+
   public hasFocus: boolean = false;
+  public isDisabled: boolean = false;
+
+  private onChange = (value: string) => {};
+  private onTouched = () => {};
+  private _value: string = '';
 
   public onKey(e: KeyboardEvent) {
-    this.textarea = (<HTMLTextAreaElement>e.target).value;
+    this.value = (<HTMLTextAreaElement>e.target).value;
   }
 
   public onBlur() {
@@ -20,6 +41,23 @@ export class TextareaComponent {
   }
 
   public onFocus() {
+    this.onTouched();
     this.hasFocus = true;
+  }
+
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  public registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  public registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  public setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 }
