c21556a4bc9593a7b2cc7f253b7d8e0a5c84da98
AVoytenko <voytenko.developer@gmail.com>
2023-08-16 01:23:29 +0300

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TextareaComponent } from './components/textarea/textarea.component';
+import { TextareaMComponent } from './components/textarea-m/textarea-m.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      declarations: [AppComponent, TextareaComponent, TextareaMComponent],
     }).compileComponents();
   });
 
